132bca67bee2f7f425dbd9384c15007567f7dc96
robogamer9849 <tahanorouzi415@gmail.com>
2025-05-03 14:22:20 +0330

    removed useless things

--- lib/main.dart
+++ lib/main.dart
@@ -233,17 +233,6 @@ class _TcpPageState extends State<TcpPage> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     
-    // Define our custom colors from the provided hex codes
-    const Color lightBlue = Color(0xFF4CC9F0);
-    const Color blue = Color(0xFF4895EF);
-    const Color indigoBlue = Color(0xFF4361EE);
-    const Color deepPurple = Color(0xFF3F37C9);
-    const Color darkPurple = Color(0xFF3A0CA3);
-    const Color purple = Color(0xFF480CA8);
-    const Color violet = Color(0xFF560BAD);
-    const Color magenta = Color(0xFF7209B7);
-    const Color pink = Color(0xFFB5179E);
-    const Color brightPink = Color(0xFFF72585);
     
     return Scaffold(
       extendBodyBehindAppBar: true,
